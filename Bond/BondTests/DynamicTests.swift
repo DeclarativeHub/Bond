a779ce2173345b0e84cc335e692cf51d54f958bf
Brian Hardy <brian@bignerdranch.com>
2015-01-30 16:51:35 -0500

    Adds test for multiple Dynamic Listeners

--- Bond/BondTests/DynamicTests.swift
+++ Bond/BondTests/DynamicTests.swift
@@ -36,5 +36,28 @@ class DynamicTests: XCTestCase {
         // assert: newValue should change
         XCTAssertEqual(newValue, 1)
     }
+    
+    func testValueChangeWithMultipleListeners() {
+        let dynamicInt = Dynamic<Int>(0)
+        var newValues = [Int]()
+        // strong reference bonds to avoid premature dealloc
+        var bonds = [Bond<Int>]()
+        for i in 0..<10 {
+            newValues.append(NSNotFound)
+            let bond = Bond<Int>({ value in
+                newValues[i] = value
+            })
+            bonds.append(bond)
+            dynamicInt.bonds.append(BondBox<Int>(bond))
+        }
+        
+        // act: change the value to 1
+        dynamicInt.value = 1
+        
+        // assert that all the listeners were notified
+        for i in 0..<10 {
+            XCTAssertEqual(newValues[i], 1, "New value at index \(i) is incorrect")
+        }
+    }
 
 }
