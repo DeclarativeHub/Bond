a3fa99832cd4606e4033652c18b58702c418e5c8
Anthony Mittaz <anthony@marketplacer.com>
2015-05-13 09:56:07 +1000

    fix spelling (chnage to change)

--- BondTests/FunctionalTests.swift
+++ BondTests/FunctionalTests.swift
@@ -55,10 +55,10 @@ class ReduceTests: XCTestCase {
     XCTAssert(r.valid == true, "Should not be faulty")
     
     d1.value = 2
-    XCTAssert(r.value == 4, "Value after first dynamic chnage")
+    XCTAssert(r.value == 4, "Value after first dynamic change")
     
     d2.value = 3
-    XCTAssert(r.value == 6, "Value after second dynamic chnage")
+    XCTAssert(r.value == 6, "Value after second dynamic change")
   }
   
   func testReduce3() {
@@ -72,13 +72,13 @@ class ReduceTests: XCTestCase {
     XCTAssert(r.valid == true, "Should not be faulty")
     
     d1.value = 2
-    XCTAssert(r.value == 12, "Value after first dynamic chnage")
+    XCTAssert(r.value == 12, "Value after first dynamic change")
     
     d2.value = 3
-    XCTAssert(r.value == 18, "Value after second dynamic chnage")
+    XCTAssert(r.value == 18, "Value after second dynamic change")
     
     d3.value = 2
-    XCTAssert(r.value == 12, "Value after third dynamic chnage")
+    XCTAssert(r.value == 12, "Value after third dynamic change")
   }
   
   func testRewrite() {
@@ -113,10 +113,10 @@ class ReduceTests: XCTestCase {
     XCTAssert(z.valid == true, "Should not be faulty")
     
     d1.value = 2
-    XCTAssert(z.value.0 == 2 && z.value.1 == 2, "Value after first dynamic chnage")
+    XCTAssert(z.value.0 == 2 && z.value.1 == 2, "Value after first dynamic change")
     
     d2.value = 3
-    XCTAssert(z.value.0 == 2 && z.value.1 == 3, "Value after second dynamic chnage")
+    XCTAssert(z.value.0 == 2 && z.value.1 == 3, "Value after second dynamic change")
   }
   
   func testSkip() {
@@ -148,11 +148,11 @@ class ReduceTests: XCTestCase {
     XCTAssert(a.valid == false, "Should be faulty")
     
     d1.value = 2
-    XCTAssert(a.value == 2, "Value after first dynamic chnage")
+    XCTAssert(a.value == 2, "Value after first dynamic change")
     XCTAssert(a.valid == true, "Should not be faulty")
     
     d2.value = 3
-    XCTAssert(a.value == 3, "Value after second dynamic chnage")
+    XCTAssert(a.value == 3, "Value after second dynamic change")
     XCTAssert(a.valid == true, "Should not be faulty")
   }
   
