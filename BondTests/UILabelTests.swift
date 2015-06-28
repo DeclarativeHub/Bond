1c1f5c1208d746a86c46adf11b1e7312d7ef2024
murrayds <dakota.s.murray@gmail.com>
2015-06-28 12:21:16 -0400

    Changed the indentation of the previously modified files from 4-space indentation to 2-space indentation so as to be consistent with the rest of the project.
    Also modified the default color of the 'dynTextColor' attribute of UILabel to be black rather than clear

--- BondTests/UILabelTests.swift
+++ BondTests/UILabelTests.swift
@@ -12,45 +12,45 @@ import Bond
 
 class UILabelTests: XCTestCase {
     
-    func testUILabelBond() {
-        var dynamicDriver = Dynamic<String>("b")
-        let label = UILabel()
+  func testUILabelBond() {
+    var dynamicDriver = Dynamic<String>("b")
+    let label = UILabel()
         
-        label.text = "a"
-        XCTAssert(label.text == "a", "Initial value")
+    label.text = "a"
+    XCTAssert(label.text == "a", "Initial value")
         
-        dynamicDriver ->> label.designatedBond
-        XCTAssert(label.text == "b", "Value after binding")
+    dynamicDriver ->> label.designatedBond
+    XCTAssert(label.text == "b", "Value after binding")
         
-        dynamicDriver.value = "c"
-        XCTAssert(label.text == "c", "Value after dynamic change")
-    }
+    dynamicDriver.value = "c"
+    XCTAssert(label.text == "c", "Value after dynamic change")
+  }
     
-    func testUILabelAttributedTextBond() {
-        var dynamicDriver = Dynamic<NSAttributedString>(NSAttributedString(string: "b"))
-        let label = UILabel()
+  func testUILabelAttributedTextBond() {
+    var dynamicDriver = Dynamic<NSAttributedString>(NSAttributedString(string: "b"))
+    let label = UILabel()
         
-        label.text = "a"
-        XCTAssert(label.text == "a", "Initial value")
+    label.text = "a"
+    XCTAssert(label.text == "a", "Initial value")
         
-        dynamicDriver ->> label.dynAttributedText
-        XCTAssert(label.attributedText.string == "b", "Value after binding")
+    dynamicDriver ->> label.dynAttributedText
+    XCTAssert(label.attributedText.string == "b", "Value after binding")
         
-        dynamicDriver.value = NSAttributedString(string: "c")
-        XCTAssert(label.attributedText.string == "c", "Value after dynamic change")
-    }
+    dynamicDriver.value = NSAttributedString(string: "c")
+    XCTAssert(label.attributedText.string == "c", "Value after dynamic change")
+  }
     
-    func testUILabelTextColorBond() {
-        var dynamicDriver = Dynamic<UIColor>(UIColor.blackColor())
-        var label = UILabel()
+  func testUILabelTextColorBond() {
+    var dynamicDriver = Dynamic<UIColor>(UIColor.blackColor())
+    var label = UILabel()
         
-        label.textColor = UIColor.redColor()
-        XCTAssert(label.textColor == UIColor.redColor(), "Initial Value")
+    label.textColor = UIColor.redColor()
+    XCTAssert(label.textColor == UIColor.redColor(), "Initial Value")
         
-        dynamicDriver ->> label.dynTextColor
-        XCTAssert(label.textColor == UIColor.blackColor(), "Value after binding")
+    dynamicDriver ->> label.dynTextColor
+    XCTAssert(label.textColor == UIColor.blackColor(), "Value after binding")
         
-        dynamicDriver.value = UIColor.blueColor()
-        XCTAssert(label.textColor == UIColor.blueColor(), "Value after dynamic change")
-    }
+    dynamicDriver.value = UIColor.blueColor()
+    XCTAssert(label.textColor == UIColor.blueColor(), "Value after dynamic change")
+  }
 }
