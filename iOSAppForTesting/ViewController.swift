8a2cce1568a1a655c96670b2aebd459945a24384
bytelee <bytegm@gmail.com>
2015-10-10 11:07:22 +0800

    add bnd_editing for UITextFiled and tested

--- iOSAppForTesting/ViewController.swift
+++ iOSAppForTesting/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Bond
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    self.textField.bnd_editing.observeNew({editing in
+      if editing{
+        print("begin editing")
+      }else{
+        print("end editing")
+      }
+    })
+    
+    let bgBtn = UIButton(type: UIButtonType.Custom)
+    bgBtn.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame))
+    bgBtn.bnd_tap.observe { () -> () in
+      self.textField.resignFirstResponder()
+    }
+    self.view.addSubview(bgBtn)
+    self.view.bringSubviewToFront(self.textField)
   }
 
   override func didReceiveMemoryWarning() {
