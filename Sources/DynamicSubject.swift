bf6e637acd874421085e2729d2c325dadadbc8d1
Anthony Miller <anthony@app-order.com>
2016-10-10 18:33:37 -0700

    Added external parameter names

--- Sources/DynamicSubject.swift
+++ Sources/DynamicSubject.swift
@@ -95,9 +95,10 @@ public struct DynamicSubject<Target: Deallocatable, Element>: SubjectProtocol, B
   }
   
   /// Transform the `getter` and `setter` by applying a `transform` on them.
-  public func bidirectionalMap<U>(_ getTransform: @escaping (Element) -> U,
-                               _ setTransform: @escaping (U) -> Element) -> DynamicSubject<Target, U>! {
+  public func bidirectionalMap<U>(to getTransform: @escaping (Element) -> U,
+                               from setTransform: @escaping (U) -> Element) -> DynamicSubject<Target, U>! {
     guard let target = target else { return nil }
+    
     return DynamicSubject<Target, U>(target: target,
                                      signal: signal,
                                      get: { [getter] (target) -> U in
