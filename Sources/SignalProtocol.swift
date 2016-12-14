6be6cfefab9a8b4ec9bbd00b72b6f69df01a4634
Srdan Rasic <srdan.rasic@gmail.com>
2016-12-14 15:57:50 +0100

    Add hartbeat signal operator

---
+++ Sources/SignalProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  SignalProtocol.swift
+//  Bond
+//
+//  Created by Srdan Rasic on 14/12/2016.
+//  Copyright © 2016 Swift Bond. All rights reserved.
+//
+
+import ReactiveKit
+
+extension SignalProtocol {
+
+  /// Fires an event on start and every `interval` seconds as long as the app is in foreground.
+  /// Pauses when the app goes to background. Restarts when the app is back in foreground.
+  public static func heartbeat(interval seconds: Double) -> Signal<Void, NoError> {
+    let willEnterForeground = NotificationCenter.default.bnd_notification(name: .UIApplicationWillEnterForeground)
+    let didEnterBackgorund = NotificationCenter.default.bnd_notification(name: .UIApplicationDidEnterBackground)
+    return willEnterForeground.replace(with: ()).start(with: ()).flatMapLatest { () -> Signal<Void, NoError> in
+      return Signal<Int, NoError>.interval(seconds, queue: .global()).replace(with: ()).start(with: ()).take(until: didEnterBackgorund)
+    }
+  }
+}
