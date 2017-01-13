ee9a2b619490fc63ad81ae43c52d19bd1b02433b
Tony Arnold <tony@thecocoabots.com>
2017-01-13 20:30:37 +1100

    Add DynamicSubject support for all currently possible NSButton properties

--- Sources/AppKit/NSButton.swift
+++ Sources/AppKit/NSButton.swift
@@ -27,15 +27,176 @@ import ReactiveKit
 
 public extension ReactiveExtensions where Base: NSButton {
 
-  public var title: Bond<String> {
-    return bond { $0.title = $1 }
+  public var title: DynamicSubject<String> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.title), ofType: String.self).eraseType(),
+      get: { $0.title },
+      set: { $0.title = $1 }
+    )
   }
 
-  public var alternateTitle: Bond<String> {
-    return bond { $0.alternateTitle = $1 }
+  public var alternateTitle: DynamicSubject<String> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.alternateTitle), ofType: String.self).eraseType(),
+      get: { $0.alternateTitle },
+      set: { $0.alternateTitle = $1 }
+    )
   }
 
-  public var state: Bond<Int> {
-    return bond { $0.state = $1 }
+  public var image: DynamicSubject<NSImage?> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.image), ofType: Optional<NSImage>.self).eraseType(),
+      get: { $0.image },
+      set: { $0.image = $1 }
+    )
+  }
+
+  public var alternateImage: DynamicSubject<NSImage?> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.alternateImage), ofType: Optional<NSImage>.self).eraseType(),
+      get: { $0.alternateImage },
+      set: { $0.alternateImage = $1 }
+    )
+  }
+
+  public var state: DynamicSubject<Int> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.state), ofType: Int.self).eraseType(),
+      get: { $0.state },
+      set: { $0.state = $1 }
+    )
+  }
+
+  public var imagePosition: DynamicSubject<NSCellImagePosition> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.imagePosition), ofType: NSCellImagePosition.self).eraseType(),
+      get: { $0.imagePosition },
+      set: { $0.imagePosition = $1 }
+    )
+  }
+
+  public var imageScaling: DynamicSubject<NSImageScaling> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.imageScaling), ofType: NSImageScaling.self).eraseType(),
+      get: { $0.imageScaling },
+      set: { $0.imageScaling = $1 }
+    )
+  }
+
+  @available(macOS 10.12, *)
+  public var imageHugsTitle: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.imageHugsTitle), ofType: Bool.self).eraseType(),
+      get: { $0.imageHugsTitle },
+      set: { $0.imageHugsTitle = $1 }
+    )
+  }
+
+  public var isBordered: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.isBordered), ofType: Bool.self).eraseType(),
+      get: { $0.isBordered },
+      set: { $0.isBordered = $1 }
+    )
+  }
+
+  public var isTransparent: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.isTransparent), ofType: Bool.self).eraseType(),
+      get: { $0.isTransparent },
+      set: { $0.isTransparent = $1 }
+    )
+  }
+
+  public var keyEquivalent: DynamicSubject<String> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.keyEquivalent), ofType: String.self).eraseType(),
+      get: { $0.keyEquivalent },
+      set: { $0.keyEquivalent = $1 }
+    )
+  }
+
+  public var keyEquivalentModifierMask: DynamicSubject<NSEventModifierFlags> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.keyEquivalentModifierMask), ofType: NSEventModifierFlags.self).eraseType(),
+      get: { $0.keyEquivalentModifierMask },
+      set: { $0.keyEquivalentModifierMask = $1 }
+    )
+  }
+
+  @available(macOS 10.10.3, *)
+  public var isSpringLoaded: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.isSpringLoaded), ofType: Bool.self).eraseType(),
+      get: { $0.isSpringLoaded },
+      set: { $0.isSpringLoaded = $1 }
+    )
+  }
+
+  @available(macOS 10.10.3, *)
+  public var maxAcceleratorLevel: DynamicSubject<Int> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.maxAcceleratorLevel), ofType: Int.self).eraseType(),
+      get: { $0.maxAcceleratorLevel },
+      set: { $0.maxAcceleratorLevel = $1 }
+    )
+  }
+
+  @available(macOS 10.12.2, *)
+  public var bezelColor: DynamicSubject<NSColor?> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.bezelColor), ofType: Optional<NSColor>.self).eraseType(),
+      get: { $0.bezelColor },
+      set: { $0.bezelColor = $1 }
+    )
+  }
+
+  public var attributedTitle: DynamicSubject<NSAttributedString> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.attributedTitle), ofType: NSAttributedString.self).eraseType(),
+      get: { $0.attributedTitle },
+      set: { $0.attributedTitle = $1 }
+    )
+  }
+
+  public var attributedAlternateTitle: DynamicSubject<NSAttributedString> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.attributedAlternateTitle), ofType: NSAttributedString.self).eraseType(),
+      get: { $0.attributedAlternateTitle },
+      set: { $0.attributedAlternateTitle = $1 }
+    )
+  }
+
+  public var bezelStyle: DynamicSubject<NSBezelStyle> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.bezelStyle), ofType: Optional<NSBezelStyle>.self).eraseType(),
+      get: { $0.bezelStyle },
+      set: { $0.bezelStyle = $1 }
+    )
+  }
+
+  public var allowsMixedState: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.allowsMixedState), ofType: Optional<Bool>.self).eraseType(),
+      get: { $0.allowsMixedState },
+      set: { $0.allowsMixedState = $1 }
+    )
+  }
+
+  public var showsBorderOnlyWhileMouseInside: DynamicSubject<Bool> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.showsBorderOnlyWhileMouseInside), ofType: Optional<Bool>.self).eraseType(),
+      get: { $0.showsBorderOnlyWhileMouseInside },
+      set: { $0.showsBorderOnlyWhileMouseInside = $1 }
+    )
+  }
+
+  public var sound: DynamicSubject<NSSound?> {
+    return dynamicSubject(
+      signal: keyPath(#keyPath(NSButton.sound), ofType: Optional<NSSound>.self).eraseType(),
+      get: { $0.sound },
+      set: { $0.sound = $1 }
+    )
   }
 }
+
