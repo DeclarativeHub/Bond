2bd5c317d8108eb8db955ae57d74040185c10f4e
Tony Arnold <tony@thecocoabots.com>
2017-07-20 09:08:33 +1000

    Update Swift PM package to Swift 4.0 format

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "Bond",
-  targets: [
-    Target(name: "BNDProtocolProxyBase"),
-    Target(name: "Bond", dependencies: ["BNDProtocolProxyBase"])
-  ],
   dependencies: [
-    .Package(url: "https://github.com/ReactiveKit/ReactiveKit.git", versions: Version(3, 5, 3)..<Version(4, 0, 0)),
-    .Package(url: "https://github.com/wokalski/Diff.swift.git", versions: Version(0, 5, 3)..<Version(1, 0, 0))
+    .package(url: "https://github.com/ReactiveKit/ReactiveKit.git", .upToNextMajor(from: "3.5.3")),
+    .package(url: "https://github.com/wokalski/Diff.swift.git", .branch("swift-4.0"))
+  ],
+  targets: [
+    .target(name: "BNDProtocolProxyBase"),
+    .target(name: "Bond", dependencies: ["BNDProtocolProxyBase"])
   ]
 )
