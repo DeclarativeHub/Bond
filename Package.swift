425ce2fa104440b6c8ba3c25df2d742b5f31597c
Tony Arnold <tony@thecocoabots.com>
2017-08-09 23:50:28 +1000

    Move to ReactiveKit’s Swift 4 branch

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
   name: "Bond",
   dependencies: [
-    .package(url: "https://github.com/ReactiveKit/ReactiveKit.git", .upToNextMajor(from: "3.5.3")),
+    .package(url: "https://github.com/ReactiveKit/ReactiveKit.git", .branch("swift-4")),
     .package(url: "https://github.com/wokalski/Diff.swift.git", .branch("swift-4.0"))
   ],
   targets: [
     .target(name: "BNDProtocolProxyBase"),
-    .target(name: "Bond", dependencies: ["BNDProtocolProxyBase"])
+    .target(name: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKit", "Diff"])
   ]
 )
