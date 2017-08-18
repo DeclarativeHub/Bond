a615b67ab57013099e61e272a866a2dda3651684
Srdan Rasic <srdan.rasic@gmail.com>
2017-08-18 10:24:23 +0200

    Bump version|

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     Target(name: "Bond", dependencies: ["BNDProtocolProxyBase"])
   ],
   dependencies: [
-    .Package(url: "https://github.com/ReactiveKit/ReactiveKit.git", versions: Version(3, 5, 3)..<Version(4, 0, 0)),
+    .Package(url: "https://github.com/ReactiveKit/ReactiveKit.git", versions: Version(3, 6, 0)..<Version(4, 0, 0)),
     .Package(url: "https://github.com/wokalski/Diff.swift.git", versions: Version(0, 5, 3)..<Version(1, 0, 0))
   ]
 )
