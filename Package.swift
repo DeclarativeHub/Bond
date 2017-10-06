a3b32bdbb60dda383a16b4577a24570532818f7d
Tony Arnold <tony@thecocoabots.com>
2017-10-06 12:57:58 +1100

    Add a library product for Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -3,12 +3,16 @@ import PackageDescription
 
 let package = Package(
   name: "Bond",
+  products: [
+    .library(name: "Bond", targets: ["Bond"])
+  ],
   dependencies: [
     .package(url: "https://github.com/ReactiveKit/ReactiveKit.git", .upToNextMajor(from: "3.7.0")),
     .package(url: "https://github.com/tonyarnold/Differ.git", .upToNextMajor(from: "1.0.0"))
   ],
   targets: [
     .target(name: "BNDProtocolProxyBase"),
-    .target(name: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKit", "Differ"])
+    .target(name: "Bond", dependencies: ["BNDProtocolProxyBase", "ReactiveKit", "Differ"]),
+    .testTarget(name: "BondTests", dependencies: ["Bond"])
   ]
 )
