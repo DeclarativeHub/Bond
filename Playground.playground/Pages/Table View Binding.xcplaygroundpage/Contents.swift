1b37da9ca35d3cc034a4ee4de073841b572cb4cd
Srdan Rasic <srdan.rasic@gmail.com>
2018-03-25 15:01:39 +0200

    Clean up playgrounds

--- Playground.playground/Pages/Table View Binding.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Table View Binding.xcplaygroundpage/Contents.swift
@@ -4,25 +4,5 @@ import UIKit
 import Bond
 import ReactiveKit
 import PlaygroundSupport
-import CoreLocation
 
-// Turn on the Assistant Editor to see the table view!
-
-extension ReactiveExtensions where Base: CLLocationManager {
-
-    public var delegate: ProtocolProxy {
-        return protocolProxy(for: CLLocationManagerDelegate.self, keyPath: \.delegate)
-    }
-
-    public var locations: SafeSignal<[CLLocation]> {
-        return delegate.signal(
-            for: #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)),
-            dispatch: { (subject: SafePublishSubject<[CLLocation]>, locationManager: CLLocationManager, locations: [CLLocation]) in
-                subject.next(locations)
-            }
-        )
-    }
-}
-
-//: [Next](@next)
-
+// Play here!
