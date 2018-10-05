4617307418689ff819fbfc5eae6fbd2619cfd52a
Srđan Rašić <srdan.rasic@gmail.com>
2018-10-05 22:43:13 +0200

    Add extensions for unordered collection.

--- Sources/Bond/Observable Collections/UnorderedChangeset.swift
+++ Sources/Bond/Observable Collections/UnorderedChangeset.swift
@@ -54,3 +54,75 @@ public struct UnorderedChangeset<Collection: Swift.Collection>: UnorderedChanges
         return self
     }
 }
+
+extension ChangesetContainerProtocol where Changeset: UnorderedChangesetProtocol, Changeset.Collection: MutableCollection {
+
+    /// Access or update the element at `index`.
+    public subscript(index: Collection.Index) -> Collection.Element {
+        get {
+            return collection[index]
+        }
+        set {
+            descriptiveUpdate { (collection) -> [Operation] in
+                collection[index] = newValue
+                return [.update(at: index, newElement: newValue)]
+            }
+        }
+    }
+}
+
+extension ChangesetContainerProtocol where Changeset: UnorderedChangesetProtocol, Changeset.Collection: RangeReplaceableCollection {
+
+    /// Append `newElement` to the collection.
+    public func append(_ newElement: Collection.Element) {
+        descriptiveUpdate { (collection) -> [Operation] in
+            collection.append(newElement)
+            return [.insert(newElement, at: collection.index(collection.endIndex, offsetBy: -1))]
+        }
+    }
+
+    /// Insert `newElement` at index `i`.
+    public func insert(_ newElement: Collection.Element, at index: Collection.Index) {
+        descriptiveUpdate { (collection) -> [Operation] in
+            collection.insert(newElement, at: index)
+            return [.insert(newElement, at: index)]
+        }
+    }
+
+    /// Insert elements `newElements` at index `i`.
+    public func insert(contentsOf newElements: [Collection.Element], at index: Collection.Index) {
+        descriptiveUpdate { (collection) -> [Operation] in
+            collection.insert(contentsOf: newElements, at: index)
+            let indices = (0..<newElements.count).map { collection.index(index, offsetBy: $0) }
+            return indices.map { Operation.insert(collection[$0], at: $0) }
+        }
+    }
+
+    /// Remove and return the element at index i.
+    @discardableResult
+    public func remove(at index: Collection.Index) -> Collection.Element {
+        return descriptiveUpdate { (collection) -> ([Operation], Collection.Element) in
+            let element = collection.remove(at: index)
+            return ([.delete(at: index)], element)
+        }
+    }
+
+    /// Remove an element from the end of the collection in O(1).
+    @discardableResult
+    public func removeLast() -> Collection.Element {
+        return descriptiveUpdate { (collection) -> ([Operation], Collection.Element) in
+            let index = collection.index(collection.endIndex, offsetBy: -1)
+            let element = collection.remove(at: index)
+            return ([.delete(at: index)], element)
+        }
+    }
+
+    /// Remove all elements from the collection.
+    public func removeAll() {
+        descriptiveUpdate { (collection) -> [Operation] in
+            let deletes = collection.indices.reversed().map { Operation.delete(at: $0) }
+            collection.removeAll(keepingCapacity: false)
+            return deletes
+        }
+    }
+}
