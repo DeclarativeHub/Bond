ba23cbc3e14c1fb2d558403f45bd52c1b7e8ba92
Srđan Rašić <srdan.rasic@gmail.com>
2019-02-17 12:03:37 +0100

    Add the set of mutating methods to Array2D.

--- Sources/Bond/Data Structures/Array2D.swift
+++ Sources/Bond/Data Structures/Array2D.swift
@@ -32,21 +32,6 @@ import Foundation
 /// Signals that emit this kind of a tree can be bound to a table or collection view.
 public typealias Array2D<Section, Item> = TreeArray<Array2DElement<Section, Item>>
 
-extension TreeArray where ChildValue: Array2DElementProtocol {
-
-    public init(sectionsWithItems: [(ChildValue.Section, [ChildValue.Item])]) {
-        self.init(sectionsWithItems.map { TreeNode(ChildValue(section: $0.0), $0.1.map { TreeNode(ChildValue(item: $0)) }) })
-    }
-
-    public subscript(itemAt index: IndexPath) -> ChildValue.Item {
-        return self[index].value.item!
-    }
-
-    public subscript(sectionAt index: Int) -> ChildValue.Section {
-        return self[[index]].value.section!
-    }
-}
-
 /// A section or an item in a 2D array.
 public enum Array2DElement<Section, Item>: Array2DElementProtocol {
 
@@ -93,4 +78,88 @@ public protocol Array2DElementProtocol {
     var section: Section? { get }
     var item: Item?  { get }
     var asArray2DElement: Array2DElement<Section, Item> { get }
+}
+
+extension TreeArray where ChildValue: Array2DElementProtocol {
+
+    public init(sectionsWithItems: [(ChildValue.Section, [ChildValue.Item])]) {
+        self.init(sectionsWithItems.map { TreeNode(ChildValue(section: $0.0), $0.1.map { TreeNode(ChildValue(item: $0)) }) })
+    }
+
+    public subscript(itemAt indexPath: IndexPath) -> ChildValue.Item {
+        get {
+            return self[indexPath].value.item!
+        }
+        set {
+            self[indexPath].value = ChildValue(item: newValue)
+        }
+    }
+
+    public subscript(sectionAt index: Int) -> ChildValue.Section {
+        get {
+            return self[[index]].value.section!
+        }
+        set {
+            self[[index]].value = ChildValue(section: newValue)
+        }
+    }
+
+    /// Append new section at the end of the 2D array.
+    public mutating func appendSection(_ section: ChildValue.Section) {
+        append(TreeNode(ChildValue(section: section)))
+    }
+
+    /// Append `item` to the section `section` of the array.
+    public mutating func appendItem(_ item: ChildValue.Item, toSectionAt sectionIndex: Int) {
+        insert(item: item, at: [sectionIndex, self[[sectionIndex]].children.count])
+    }
+
+    /// Insert section at `index` with `items`.
+    public mutating func insert(section: ChildValue.Section, at index: Int)  {
+        insert(TreeNode(ChildValue(section: section)), at: [index])
+    }
+
+    /// Insert `item` at `indexPath`.
+    public mutating func insert(item: ChildValue.Item, at indexPath: IndexPath)  {
+        insert(TreeNode(ChildValue(item: item)), at: indexPath)
+    }
+
+    /// Insert `items` at index path `indexPath`.
+    public mutating func insert(contentsOf items: [ChildValue.Item], at indexPath: IndexPath) {
+        insert(contentsOf: items.map { TreeNode(ChildValue(item: $0)) }, at: indexPath)
+    }
+
+    /// Move the section at index `fromIndex` to index `toIndex`.
+    public mutating func moveSection(from fromIndex: Int, to toIndex: Int) {
+        move(from: [fromIndex], to: [toIndex])
+    }
+
+    /// Move the item at `fromIndexPath` to `toIndexPath`.
+    public mutating func moveItem(from fromIndexPath: IndexPath, to toIndexPath: IndexPath) {
+        move(from: fromIndexPath, to: toIndexPath)
+    }
+
+    /// Remove and return the section at `index`.
+    @discardableResult
+    public mutating func removeSection(at index: Int) -> ChildValue.Section {
+        return remove(at: [index]).value.section!
+    }
+
+    /// Remove and return the item at `indexPath`.
+    @discardableResult
+    public mutating func removeItem(at indexPath: IndexPath) -> ChildValue.Item {
+        return remove(at: indexPath).value.item!
+    }
+
+    /// Remove all items from the array. Keep empty sections.
+    public mutating func removeAllItems() {
+        for index in indices {
+            self[index].removeAll()
+        }
+    }
+
+    /// Remove all items and sections from the array (aka `removeAll`).
+    public mutating func removeAllItemsAndSections() {
+        removeAll()
+    }
 }
