b1dc81a05764943630d3649c45e4bae871cb8c00
Tony Arnold <tony@thecocoabots.com>
2019-06-30 21:36:56 +1000

    Use ReactiveKit v3.13.0 API in the NSGestureRecognizer extensions

--- Sources/Bond/AppKit/NSGestureRecognizer.swift
+++ Sources/Bond/AppKit/NSGestureRecognizer.swift
@@ -42,12 +42,12 @@ extension ReactiveExtensions where Base: NSView {
         let base = self.base
         return Signal { [weak base] observer in
             guard let base = base else {
-                observer.completed()
+                observer.receive(completion: .finished)
                 return NonDisposable.instance
             }
             let target = BNDGestureTarget(view: base, gestureRecognizer: gestureRecognizer) { recog in
                 // swiftlint:disable:next force_cast
-                observer.next(recog as! T)
+                observer.receive(recog as! T)
             }
             return BlockDisposable {
                 target.unregister()
