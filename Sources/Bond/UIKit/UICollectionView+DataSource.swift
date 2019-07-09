0555de0c7e961ee6b059dd9c8c512b0fd8903a85
Peter Ovchinnikov <peter@devteamforyou.com>
2019-07-09 12:50:34 +0200

    Apply changeset diff inside perform batch update.

--- Sources/Bond/UIKit/UICollectionView+DataSource.swift
+++ Sources/Bond/UIKit/UICollectionView+DataSource.swift
@@ -128,14 +128,20 @@ open class CollectionViewBinderDataSource<Changeset: SectionedDataSourceChangese
 
     public var createCell: ((Changeset.Collection, IndexPath, UICollectionView) -> UICollectionViewCell)?
 
-    public var changeset: Changeset? = nil {
-        didSet {
-            if let changeset = changeset, oldValue != nil {
+    private var _changeset: Changeset? = nil
+
+    public var changeset: Changeset? {
+        set {
+            if let changeset = newValue, _changeset != nil {
                 applyChangeset(changeset)
             } else {
+                _changeset = changeset
                 collectionView?.reloadData()
                 ensureCollectionViewSyncsWithTheDataSource()
             }
+        }
+        get {
+            return _changeset
         }
     }
 
@@ -181,11 +187,14 @@ open class CollectionViewBinderDataSource<Changeset: SectionedDataSourceChangese
         }
 
         if diff.isReload {
+            _changeset = changeset
             collectionView.reloadData()
         } else if diff.count == 1 {
+            _changeset = changeset
             applyChangesetDiff(diff)
         } else {
             collectionView.performBatchUpdates({
+                _changeset = changeset
                 applyChangesetDiff(diff)
             }, completion: nil)
         }
